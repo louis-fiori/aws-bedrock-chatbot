991048c067bb75580956eb1e9eb6de102f53d149
Louis Fiori <170877473+louis-fiori@users.noreply.github.com>
2026-06-17 17:11:32 +0200

    refactor(iam): use policy attachments instead of deprecated managed_policy_arns

--- modules/iam_role/main.tf
+++ modules/iam_role/main.tf
@@ -13,7 +13,15 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "_" {
-  name                = var.name
-  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
-  managed_policy_arns = var.managed_policy_arns
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# `managed_policy_arns` on aws_iam_role is deprecated; use explicit attachments.
+# count (not for_each) is used because the ARNs are not known until apply.
+resource "aws_iam_role_policy_attachment" "_" {
+  count = length(var.managed_policy_arns)
+
+  role       = aws_iam_role._.name
+  policy_arn = var.managed_policy_arns[count.index]
 }
